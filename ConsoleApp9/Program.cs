ed1242891f607b3ec4d5ff19b321ef103dd9bd9f
Carlos Oliveros <caoa143@uab.edu.com>
2018-07-18 10:28:54 -0500

    finished

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -6,7 +6,25 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Enter the name of your lemonade stand");
+            string standName = Console.ReadLine();
+
+            Console.WriteLine("How many cups will you sell?");
+            int numCups = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many Customers do you predict?");
+            int numCustomers = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much does each cup cost?");
+            int priceCups = Int32.Parse(Console.ReadLine());
+
+            int totalRevenue = numCups * priceCups;
+
+            Console.WriteLine("Your total revennue would be"+totalRevenue);
+
+            Console.ReadLine();
+
         }
     }
 }
