de6b61f8321173302da0002be51a2968835cdeda
Carlos Oliveros <caoa143@uab.edu.com>
2018-07-19 12:27:52 -0500

    added 2 instances Stands

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -37,7 +37,7 @@ namespace LemonadeStand
             lemonadeStand.PriceCups = decimal.Parse(Console.ReadLine());//***************
 
             Console.WriteLine("How much will each cup cost to make? ");
-            lemonadeStand.CostperCup = decimal.Parse(Console.ReadLine());
+            lemonadeStand.CostperCup = decimal.Parse(Console.ReadLine()); //***************
 
             //___________________
             decimal totalRevenue = lemonadeStand.NumbCups * lemonadeStand.PriceCups;
@@ -54,6 +54,35 @@ namespace LemonadeStand
             Console.WriteLine("Your total expenses would be" + totalProfit);
 
             Console.ReadLine();
+
+            //_+________________________________________-
+
+            LemonadeStand stand2 = new LemonadeStand();
+
+            stand2.Name = Console.ReadLine();
+            stand2.NumbCups= Int32.Parse(Console.ReadLine());
+            stand2.PriceCups = decimal.Parse(Console.ReadLine());
+            stand2.CostperCup = decimal.Parse(Console.ReadLine());
+            stand2.NumCostumers= Int32.Parse(Console.ReadLine());
+
+            decimal revenue2 = stand2.PriceCups * stand2.NumCostumers;
+            decimal totalExpenses2 = stand2.NumbCups * stand2.CostperCup;
+            decimal totalProfit2 = revenue2 - totalExpenses2;
+
+            //________________________________3____________________________
+
+            LemonadeStand stand3 = new LemonadeStand();
+
+            stand3.Name = Console.ReadLine();
+            stand3.NumbCups= Int32.Parse(Console.ReadLine());
+            stand3.PriceCups= decimal.Parse(Console.ReadLine());
+            stand3.CostperCup= decimal.Parse(Console.ReadLine());
+            stand3.NumCostumers= Int32.Parse(Console.ReadLine());
+
+            decimal revenue3 = stand3.PriceCups * stand3.NumCostumers;
+            decimal totalExpenses3 = stand3.NumbCups * stand3.CostperCup;
+            decimal totalProfit3 = revenue2 - totalExpenses2;
+
 
         }
     }
