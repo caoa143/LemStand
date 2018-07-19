805b2e79c362d725edba788c1b2467cd36c24e4f
Carlos Oliveros <caoa143@uab.edu.com>
2018-07-19 09:19:45 -0500

    Added LemStand Class

---
+++ ConsoleApp9/LemonadeStand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    class LemonadeStand
+    {
+        public string Name { get; set; }
+
+        public int PriceCups { get; set; }
+
+        public int NumbCups { get; set; }
+
+
+
+
+    }
+}
